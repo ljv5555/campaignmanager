e46de18040045e021dc287535faa066a019d5f1e
ljv5555 <ljv5555@users.noreply.github.com>
2016-12-28 18:54:06 -0600

    v3 reduces choice time

--- src/ChooserImplTest.java
+++ src/ChooserImplTest.java
@@ -13,7 +13,7 @@ public class ChooserImplTest {
 	@Test
 	public void testIterationTimes()
 	{
-		for(int i=10;i<=80;i+=10)
+		for(int i=10;i<=40;i+=10)
 		{
 			System.out.println("v1-collection size: "+i+", average choice time (ms): "+testChoices(i, 1000000));
 		}
@@ -22,9 +22,18 @@ public class ChooserImplTest {
 	@Test
 	public void testIterationTimes2()
 	{
-		for(int i=10;i<=80;i+=10)
+		for(int i=10;i<=40;i+=10)
 		{
 			System.out.println("v2-collection size: "+i+", average choice time (ms): "+testChoices2(i, 500000));
+		}
+	}
+	
+	@Test
+	public void testIterationTimes3()
+	{
+		for(int i=10;i<=40;i+=10)
+		{
+			System.out.println("v3-collection size: "+i+", average choice time (ms): "+testChoices3(i, 500000));
 		}
 	}
 	
@@ -145,6 +154,62 @@ public class ChooserImplTest {
 		return averageTime/1000.0;
 		
 	}
+	
+	public double testChoices3(int totalChoices, int iterations) {
+		List<Integer> choiceTimesNs = new ArrayList<Integer>();
+		double variance = 0.10;
+		Chooser<String> c = new ChooserImpl3<String>();
+		List<Double> weights =  new ArrayList<>();
+		List<String> items = new ArrayList<>();
+		for(int i=0;i<totalChoices;i++)
+		{
+			String item = "choice_"+i;
+			Double weight = 1.0*(i+1);
+			c.add(item,weight);
+			items.add(item);
+			weights.add(weight);
+		}
+		
+		
+		double totalWeight = 0;
+		
+		List<Double> proportions = new ArrayList<Double>();
+		
+		for(Double w : weights){totalWeight+=w;proportions.add(w);}
+		for(int i=0;i<proportions.size();i++){double d = proportions.get(i)/totalWeight;proportions.set(i, new Double(d));}
+		//System.out.println("1.");
+		List<String> choices = new ArrayList<String>();
+		List<Double> itemChosenCount = new ArrayList<Double>();
+		for(String item : items){itemChosenCount.add(0.0);}
+		for(int i=0;i<iterations;i++)
+		{
+			long prevtime = System.nanoTime();
+			String choice = c.choose();
+			choiceTimesNs.add(new Integer((int)(System.nanoTime()-prevtime)));
+			choices.add(choice);
+			Double prevItemChosenCount=itemChosenCount.get(items.indexOf(choice));
+			itemChosenCount.set(items.indexOf(choice), prevItemChosenCount+1.0);
+		}
+	
+		for(int i=0;i<items.size();i++)
+		{
+			double expectedItemCount = iterations * proportions.get(i);
+			double expectedItemCountLow = expectedItemCount - (expectedItemCount * variance);
+			double expectedItemCountHigh = expectedItemCount + (expectedItemCount * variance);
+			int count = Collections.frequency(choices, items.get(i));
+			//System.out.println("i="+i+",   "+expectedItemCountLow + " <= "+count + " <= "+expectedItemCountHigh );
+			//assertTrue(count>=expectedItemCountLow);
+			//assertTrue(count<=expectedItemCountHigh);
+		}
+		double totalTime = 0;
+		for(Integer i : choiceTimesNs)
+		{
+			totalTime+=i;
+		}
+		double averageTime = totalTime / choiceTimesNs.size();
+		return averageTime/1000.0;
+		
+	}
 
 	@Test
 	public void test1() {
@@ -173,6 +238,29 @@ public class ChooserImplTest {
 	@Test
 	public void test2() {
 		Chooser<String> c = new ChooserImpl2<String>();
+		c.add("a", 5);
+		c.add("b", 2.5);
+		c.add("c", 2.5);
+		String choices = "";
+		for(int i=0;i<1000;i++){choices=choices+c.choose();}
+		int countA = choices.replaceAll("[^a]", "").length();
+		int countB = choices.replaceAll("[^b]", "").length();
+		int countC = choices.replaceAll("[^c]", "").length();
+		assertTrue((countA+countB+countC)==1000);
+		System.out.println(choices);
+		
+		System.out.println("countA:"+countA+", countB="+countB+", countC="+countC);
+		assertTrue(countA>(500-(.25*500)));
+		assertTrue(countA<(500+(.25*500)));
+		assertTrue(countB>(250-(.25*250)));
+		assertTrue(countB<(250+(.25*250)));
+		assertTrue(countC>(250-(.25*250)));
+		assertTrue(countC<(250+(.25*250)));
+		
+	}
+	@Test
+	public void test3() {
+		Chooser<String> c = new ChooserImpl3<String>();
 		c.add("a", 5);
 		c.add("b", 2.5);
 		c.add("c", 2.5);
